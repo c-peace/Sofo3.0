bb06f44dc0cc78627b75df02223ee881605ddd63
c-peace <85606158+c-peace@users.noreply.github.com>
2023-09-26 11:32:53 +0000

    Feat: Link buttons, Style: change px to rem

--- src/features/nav/Nav.js
+++ src/features/nav/Nav.js
@@ -5,10 +5,10 @@ export default function Nav() {
         <nav>
             <h1 id='logo'>Sofo</h1>
             <div id='linkBtnBox'>
-                <a title='Home' className='linkBtn' href="http://intro.sofomusic.com" target='_blank'>Home</a>
-                <a className='linkBtn' href="http://intro.sofomusic.com" target='_blank'>Sample</a>
-                <a className='linkBtn' href="http://intro.sofomusic.com" target='_blank'>FeedBack</a>
-                <a className='linkBtn' href="http://intro.sofomusic.com" target='_blank'>Tutorial</a>
+                <a className='linkBtn' href="http://intro.sofomusic.com" target='_blank'>Home</a>
+                <a className='linkBtn' href="https://intro.sofomusic.com/sample" target='_blank'>Sample</a>
+                <a className='linkBtn' href="https://forms.gle/uL1nXcotBZctePyC9" target='_blank'>FeedBack</a>
+                <a className='linkBtn' href="https://intro.sofomusic.com/guide" target='_blank'>Tutorial</a>
             </div>
         </nav>
     );
