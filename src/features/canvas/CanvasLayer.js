c357ada7f0ff7b19423f9be4211662ca07d9586d
c-peace <85606158+c-peace@users.noreply.github.com>
2023-09-28 04:44:09 +0000

    FEAT: Create draw DefaultSheet Function
    react에서 이미지 경로는 import해서 가져오는 방식을 이용해야함 기본화면을 띄우는 기능만 추가해 봄

--- src/features/canvas/CanvasLayer.js
+++ src/features/canvas/CanvasLayer.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-
+import defaultSheet from "/workspaces/Sofo3.0/src/assets/defaultSheet.png";
 import './CanvasLayer.css'
 
 export default function CanavasLayer() {
@@ -9,6 +9,20 @@ export default function CanavasLayer() {
     const canvasSubmitRef = useRef();
 
     useEffect(() => {
+        const canvasMain = canvasMainRef.current;
+        const ctxMain = canvasMain.getContext('2d');
+        canvasMain.width = 1190;
+        canvasMain.height = 1684;
+        const canvasFlag = canvasFlagRef.current;
+        const ctxFlag = canvasFlag.getContext('2d');
+        canvasFlag.width = 1190;
+        canvasFlag.height = 1684;
+        const canvasSubmit = canvasSubmitRef.current;
+        const ctxSubmit = canvasSubmit.getContext('2d');
+        canvasSubmit.width = 1190;
+        canvasSubmit.height = 1684;
+        
+        drawDefaultSheet(ctxMain);
     }, []);
 
     return (
@@ -19,4 +33,12 @@ export default function CanavasLayer() {
         </div>
     );
 
+    // Main canvas - Default Setting
+    function drawDefaultSheet(ctx) {
+        const image = new Image();
+        image.src = defaultSheet;
+        image.onload = function () {
+            ctx.drawImage(image, 0, 0);
+        }
+    }
 }
