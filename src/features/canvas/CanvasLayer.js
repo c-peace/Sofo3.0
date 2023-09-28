81857284f060da682cb42cadee131a0c32fad756
c-peace <85606158+c-peace@users.noreply.github.com>
2023-09-28 05:36:44 +0000

    FEAT: Create draw flag func
    CanvasLayer 컴포넌트에 Flag를 화면에 띄우는 코드를 작성하고 화면에 띄어지는 것 까지 테스트함

--- src/features/canvas/CanvasLayer.js
+++ src/features/canvas/CanvasLayer.js
@@ -1,29 +1,48 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import defaultSheet from "/workspaces/Sofo3.0/src/assets/defaultSheet.png";
 import './CanvasLayer.css'
+
+const CANVAS = { width: 1190, height: 1684 };
+
 
 export default function CanavasLayer() {
 
     const canvasFlagRef = useRef();
     const canvasMainRef = useRef();
     const canvasSubmitRef = useRef();
+    const [flags, setFlags] = useState([]);
 
     useEffect(() => {
         const canvasMain = canvasMainRef.current;
         const ctxMain = canvasMain.getContext('2d');
-        canvasMain.width = 1190;
-        canvasMain.height = 1684;
+        canvasMain.width = CANVAS.width;
+        canvasMain.height = CANVAS.height;
+
+        drawDefaultSheet(ctxMain);
+
+
+        setFlags([{
+            x: Math.floor(Math.random() * 951) + 120,
+            y: Math.floor(Math.random() * 100) + 1500,
+            width: 50,
+            height: 50,
+            strokeStyle: "red",
+            fillStyle: "white",
+            name: 'A',
+            isDragging: false
+        }]);
+        
+    }, []);
+
+    useEffect(() => {
         const canvasFlag = canvasFlagRef.current;
         const ctxFlag = canvasFlag.getContext('2d');
-        canvasFlag.width = 1190;
-        canvasFlag.height = 1684;
-        const canvasSubmit = canvasSubmitRef.current;
-        const ctxSubmit = canvasSubmit.getContext('2d');
-        canvasSubmit.width = 1190;
-        canvasSubmit.height = 1684;
-        
-        drawDefaultSheet(ctxMain);
-    }, []);
+        canvasFlag.width = CANVAS.width;
+        canvasFlag.height = CANVAS.height;
+
+        draw(ctxFlag, flags);
+
+    }, [flags]);
 
     return (
         <div id='canvasBox'>
@@ -33,12 +52,55 @@ export default function CanavasLayer() {
         </div>
     );
 
-    // Main canvas - Default Setting
-    function drawDefaultSheet(ctx) {
+    // Main canvas
+    function drawDefaultSheet(ctxMain) {
         const image = new Image();
         image.src = defaultSheet;
         image.onload = function () {
-            ctx.drawImage(image, 0, 0);
+            ctxMain.drawImage(image, 0, 0);
+        }
+    }
+
+    // Flag canvas
+    function createFlag(name) {
+        flags.push({
+            x: Math.floor(Math.random() * 951) + 120,
+            y: Math.floor(Math.random() * 100) + 1500,
+            width: 50,
+            height: 50,
+            strokeStyle: "red",
+            fillStyle: "white",
+            name: name,
+            isDragging: false
+        });
+        draw();
+    }
+
+    function clearCanvasFlag(ctxFlag) {
+        ctxFlag.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    }
+
+    function rect(ctxFlag, r) {
+        ctxFlag.save();
+        ctxFlag.beginPath();
+        // inner fill
+        ctxFlag.fillStyle = r.fillStyle;
+        ctxFlag.fillRect(r.x, r.y, r.width, r.height);
+        // outline
+        ctxFlag.lineWidth = 3;
+        ctxFlag.strokeRect(r.x, r.y, r.width, r.height);
+        // text
+        ctxFlag.textAlign = 'center';
+        ctxFlag.fillStyle = r.strokeStyle;
+        ctxFlag.font = '37px Arial';
+        ctxFlag.fillText(r.name, r.x + 25, r.y + 39);
+        ctxFlag.restore();
+    }
+
+    function draw(ctxFlag, flags) {
+        clearCanvasFlag(ctxFlag);
+        for (let i = 0; i < flags.length; i++) {
+            rect(ctxFlag, flags[i]);
         }
     }
 }
