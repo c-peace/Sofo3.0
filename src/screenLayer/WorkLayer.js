cecc4ec94dc6d7f44a728bef56203c3ee50249b8
c-peace <85606158+c-peace@users.noreply.github.com>
2023-09-30 10:57:27 +0000

    STYLE: Set WorkLayer to inline css

--- src/screenLayer/WorkLayer.js
+++ src/screenLayer/WorkLayer.js
@@ -1,12 +1,10 @@
-import './WorkLayer.css'
-
 import Slide from '/workspaces/Sofo3.0/src/features/slide/Slide.js';
 import Canvas from '/workspaces/Sofo3.0/src/features/canvas/Canvas.js';
 import CanvasBtnR from '/workspaces/Sofo3.0/src/features/canvasBtnR/CanvasBtnR.js';
 
 export default function WorkLayer() {
   return (
-    <div id='workLayer'>
+    <div id='workLayer' style={{display:'flex', flexDirection:'row'}}>
       <Slide />
       <Canvas />
       <CanvasBtnR />
