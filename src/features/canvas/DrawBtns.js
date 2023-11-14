7a0458326a70711095305ff93b76f143d4c43ee1
Peace <cph0221@ajou.ac.kr>
2023-11-14 16:24:17 +0900

    FIX: safari dropdown Bug fix
    safari 에서는 dropdown이 동작하지 않은 Error를 수정함

--- src/features/canvas/DrawBtns.js
+++ src/features/canvas/DrawBtns.js
@@ -19,33 +19,33 @@ export function DropDownKey({ btnControls }) {
         <div className="dropdown-content-row">
 
             <div className='options'>
-                <option className="option-row-small" onClick={() => btnControls('key', 'C')}>C</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Db')}>Db</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'D')}>D</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Eb')}>Eb</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'E')}>E</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'F')}>F</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Gb')}>Gb</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'G')}>G</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Ab')}>Ab</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'A')}>A</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Bb')}>Bb</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'B')}>B</option>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'C')}>C</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Db')}>Db</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'D')}>D</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Eb')}>Eb</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'E')}>E</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'F')}>F</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Gb')}>Gb</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'G')}>G</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Ab')}>Ab</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'A')}>A</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Bb')}>Bb</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'B')}>B</div>
             </div>
 
             <div className='options'>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Cm')}>Cm</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Dbm')}>Dbm</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Dm')}>Dm</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Ebm')}>Ebm</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Em')}>Em</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Fm')}>Fm</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Gbm')}>Gbm</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Gm')}>Gm</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Abm')}>Abm</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Am')}>Am</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Bbm')}>Bbm</option>
-                <option className="option-row-small" onClick={() => btnControls('key', 'Bm')}>Bm</option>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Cm')}>Cm</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Dbm')}>Dbm</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Dm')}>Dm</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Ebm')}>Ebm</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Em')}>Em</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Fm')}>Fm</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Gbm')}>Gbm</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Gm')}>Gm</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Abm')}>Abm</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Am')}>Am</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Bbm')}>Bbm</div>
+                <div id='option' className="option-row-small" onClick={() => btnControls('key', 'Bm')}>Bm</div>
             </div>
 
         </div>
@@ -70,14 +70,14 @@ export function BtnInfo({ btnControls }) {
 export function DropDownSongform({ btnControls }) {
     return <div className="canvasBtn dropdown" id="songform">
         <div className="dropdown-content-row">
-            <option className="option-row-big" title="Intro" onClick={() => btnControls('songform', 'In')}>In</option>
-            <option className="option-row-big" title="A part" onClick={() => btnControls('songform', 'A')}>A</option>
-            <option className="option-row-big" title="B part" onClick={() => btnControls('songform', 'B')}>B</option>
-            <option className="option-row-big" title="Interlude" onClick={() => btnControls('songform', 'I')}>I</option>
-            <option className="option-row-big" title="C part" onClick={() => btnControls('songform', 'C')}>C</option>
-            <option className="option-row-big" title="D part" onClick={() => btnControls('songform', 'D')}>D</option>
-            <option className="option-row-big" title="Outro" onClick={() => btnControls('songform', 'O')}>O</option>
-            <option className="option-row-big" title="Reset" onClick={() => btnControls('eraseSongform')}>Back</option>
+            <div id='option' className="option-row-big" title="Intro" onClick={() => btnControls('songform', 'In')}>In</div>
+            <div id='option' className="option-row-big" title="A part" onClick={() => btnControls('songform', 'A')}>A</div>
+            <div id='option' className="option-row-big" title="B part" onClick={() => btnControls('songform', 'B')}>B</div>
+            <div id='option' className="option-row-big" title="Interlude" onClick={() => btnControls('songform', 'I')}>I</div>
+            <div id='option' className="option-row-big" title="C part" onClick={() => btnControls('songform', 'C')}>C</div>
+            <div id='option' className="option-row-big" title="D part" onClick={() => btnControls('songform', 'D')}>D</div>
+            <div id='option' className="option-row-big" title="Outro" onClick={() => btnControls('songform', 'O')}>O</div>
+            <div id='option' className="option-row-big" title="Reset" onClick={() => btnControls('eraseSongform')}>Back</div>
         </div>
     </div>;
 }
@@ -100,14 +100,14 @@ export function DropDownFlag({ btnControls }) {
     return <div className="dropdown canvasBtn" id="flag">
         Flag
         <div className="dropdown-content-column">
-            <option className="option-column" title="Intro" onClick={() => btnControls('flag', 'In')}>In</option>
-            <option className="option-column" title="A part" onClick={() => btnControls('flag', 'A')}>A</option>
-            <option className="option-column" title="B part" onClick={() => btnControls('flag', 'B')}>B</option>
-            <option className="option-column" title="Interlude" onClick={() => btnControls('flag', 'I')}>I</option>
-            <option className="option-column" title="C part" onClick={() => btnControls('flag', 'C')}>C</option>
-            <option className="option-column" title="D part" onClick={() => btnControls('flag', 'D')}>D</option>
-            <option className="option-column" title="Outro" onClick={() => btnControls('flag', 'O')}>O</option>
-            <option className="option-column" title="Reset" onClick={() => btnControls('eraseFlag')}>Back</option>
+            <div id='option' className="option-column" title="Intro" onClick={() => btnControls('flag', 'In')}>In</div>
+            <div id='option' className="option-column" title="A part" onClick={() => btnControls('flag', 'A')}>A</div>
+            <div id='option' className="option-column" title="B part" onClick={() => btnControls('flag', 'B')}>B</div>
+            <div id='option' className="option-column" title="Interlude" onClick={() => btnControls('flag', 'I')}>I</div>
+            <div id='option' className="option-column" title="C part" onClick={() => btnControls('flag', 'C')}>C</div>
+            <div id='option' className="option-column" title="D part" onClick={() => btnControls('flag', 'D')}>D</div>
+            <div id='option' className="option-column" title="Outro" onClick={() => btnControls('flag', 'O')}>O</div>
+            <div id='option' className="option-column" title="Reset" onClick={() => btnControls('eraseFlag')}>Back</div>
         </div>
     </div>;
 }
