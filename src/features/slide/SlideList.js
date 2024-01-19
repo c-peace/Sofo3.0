794d3564ecd50da23068759c4bc7ff2b38fcd307
Peace <cph0221@ajou.ac.kr>
2024-01-19 14:14:38 +0900

    FIX: able slide save func

--- src/features/slide/SlideList.js
+++ src/features/slide/SlideList.js
@@ -19,14 +19,12 @@ function SlideForm({ slide }) {
     const slideControl = new SlideControl(listSlide);
     const index = slide.id;
 
-    // edit 중인 slide를 눈에 띄도록 보여주는 border 스타일이다.
-    // 아직 다른 기능이 완성이 안되서 일단 적용은 안한 상태이다.
     const imgStyle = {
         border: `2px solid ${slide.edit ? '#fc5050' : '#6C6C6C'}`,
     }
 
     const handlerSlideClick = (index) => {
-        // slideControl.saveSlide();
+        slideControl.saveSlide();
         slideControl.loadSlideToCanvas(index);
     }
 
