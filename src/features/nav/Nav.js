b3e76a670b35f189f00948b497278b904cb3c0f2
Peace <cph0221@ajou.ac.kr>
2024-01-19 14:53:32 +0900

    FIX: change tutorial URL

--- src/features/nav/Nav.js
+++ src/features/nav/Nav.js
@@ -8,7 +8,7 @@ export default function Nav() {
                 <a className='linkBtn' href="http://intro.sofomusic.com" target='_blank' rel='noreferrer'>Home</a>
                 <a className='linkBtn' href="https://intro.sofomusic.com/sample" target='_blank' rel='noreferrer'>Sample</a>
                 <a className='linkBtn' href="https://forms.gle/uL1nXcotBZctePyC9" target='_blank' rel='noreferrer'>FeedBack</a>
-                <a className='linkBtn' href="https://intro.sofomusic.com/guide" target='_blank' rel='noreferrer'>Tutorial</a>
+                <a className='linkBtn' href="https://intro.sofomusic.com/tutorial" target='_blank' rel='noreferrer'>Tutorial</a>
             </div>
         </nav>
     );
